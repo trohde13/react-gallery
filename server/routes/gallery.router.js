420c36a622721a2cf8d34c4113084a5db3225bf5
Thomas Rohde <trohde@rocketmail.com>
2021-01-17 17:17:46 -0600

    set up database, wrote GET route to database with ORDER BY id DESC so that most recent photo displays first. Like button is broken, trying to retrofit PUT route

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
 const galleryItems = require('../modules/gallery.data');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
-router.put('/like/:id', (req, res) => {
+router.put(':id', (req, res) => {
     console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const queryText = `
+    UPDATE "galleryItems"
+    SET "likes" = $2
+    WHERE "id" = $1`
+    pool.query(queryText, [req.params.id, req.params.likes])
+        .then((result) => {
+            const galleryId = req.params.id;
+            for(const galleryItem of result) {   
+                if(galleryItem.id == galleryId) {
+                    galleryItem.likes += 1;
+                }
+            }
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+
+        })
+    
+    
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    const queryText = `
+    SELECT * FROM "galleryItems" 
+    ORDER BY "id" DESC;`
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            res.sendStatus(500);
+        })
+    
 }); // END GET Route
 
 module.exports = router;
